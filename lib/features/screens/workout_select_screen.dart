1b839f509f5ea8a9868e29e61ecc86d0d6cb98bb
wukinnin <nimugonzaga@gmail.com>
2026-04-24 14:39:25 +0800

    fix

--- lib/features/screens/workout_select_screen.dart
+++ lib/features/screens/workout_select_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../core/enums.dart';
 import '../../core/theme.dart';
