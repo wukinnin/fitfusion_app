43f6f3f79dba3c3755adf2ef606f4a1229e5268c
wukinnin <nimugonzaga@gmail.com>
2026-04-24 17:15:27 +0800

    bgm levels

--- lib/services/app_bgm_service.dart
+++ lib/services/app_bgm_service.dart
@@ -60,8 +60,8 @@ class AppBgmService {
 
   static const String _menuTrack = 'music/menu.mp3';
   static const String _gameTrack = 'music/game.mp3';
-  static const double _menuBgmVolume = 0.25;
-  static const double _gameBgmVolume = 0.50;
+  static const double _menuBgmVolume = 0.75;
+  static const double _gameBgmVolume = 0.5;
   static const String _volumePreferenceKey = 'global_audio_volume';
   static const int _fadeSteps = 10;
   static const Duration _fadeStepDuration = Duration(milliseconds: 50);
